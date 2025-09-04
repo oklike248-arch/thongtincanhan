3f30850af6e1ea1c7ee50717a1ef6ff129a1038e
NekoTheDev <mtmn261509@gmail.com>
2025-09-04 20:42:02 +0700

    add avatar

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -67,7 +67,7 @@ function Navbar() {
                     : "ring-cyan-400 hover:shadow-[0_0_15px_rgba(34,211,238,0.8)]"
                 }`}
             >
-              <img src="./ava.jpg" alt="avatar" className="object-cover" />
+              <img src="./public/ava.jpg" alt="avatar" className="object-cover" />
             </div>
             <span
               className={`font-extrabold text-lg tracking-wide bg-clip-text text-transparent drop-shadow-[0_0_10px_rgba(236,72,153,0.7)] ${textGradient}`}
